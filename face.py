3d9b2295875f6f7d26fb99c66ac796138113ac9e
mobeets <mobeets@gmail.com>
2017-05-20 20:13:10 -0400

    cleanup

--- face.py
+++ face.py
@@ -16,7 +16,7 @@ class Sketch:
         self.sketch = sketch
         self.doCenter = True
         self.locate(face)
-        self.pts = self.get_pts(self.sketch)
+        self.sketch = self.process(self.sketch)
 
     def locate(self, face):
         self.scale = 1.0
@@ -29,31 +29,17 @@ class Sketch:
         R = [[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]
         return np.dot(sketch, np.array(R))
 
-    def get_pts(self, sketch):
+    def process(self, sketch):
         sketch = rescale(sketch, 1.5*self.scale)
-        if True:#len(sketch.shape) > 2:
-            sketch = rotate(sketch, -90-self.rot)
-            return sketch
-        pts = np.vstack(np.where(sketch <= 0.01)).T
-        if self.doCenter:
-            med = np.min(pts, axis=0) + 0.5*(np.max(pts, axis=0) - np.min(pts, axis=0))
-        else:
-            med = np.max(pts, axis=0)
-            med[0] = np.min(pts[:,0], axis=0) + 0.5*(np.max(pts[:,0], axis=0) - np.min(pts[:,0], axis=0))
-        pts = (pts - med)
-        pts = self.rotate(pts)
-        return [pts + c for c in self.centers]
+        sketch = rotate(sketch, -90-self.rot)
+        return sketch
 
     def draw(self):
-        if True:#len(self.sketch.shape) > 2:
-            for cent in self.centers:
-                im = OffsetImage(self.pts, zoom=1)
-                ab = AnnotationBbox(im, (cent[0], cent[1]),
-                        xycoords='data', frameon=False)
-                plt.gca().add_artist(ab)
-        else:
-            for pts in self.pts:
-                plt.scatter(pts[:,0], pts[:,1], c='k', s=1)
+        for cent in self.centers:
+            im = OffsetImage(self.sketch, zoom=1)
+            ab = AnnotationBbox(im, (cent[0], cent[1]),
+                    xycoords='data', frameon=False)
+            plt.gca().add_artist(ab)
 
 class Moustache(Sketch):
     def locate(self, face):
@@ -96,10 +82,6 @@ def load_sketch(globfile):
     img = io.imread(infile)
     img = rescale(img, 0.8)
     img = rotate(img, 90)
-    # img = img.round()
-    # if len(img.shape) == 3:
-    #     img = (1-img[:,:,-1].round())
-
     if len(img.shape) == 2:
         img = np.tile(img[:,:,None], 4)
         img[:,:,-1] = 0
@@ -123,35 +105,37 @@ def draw_on_face(infile, faces, data, outfile=None):
     else:
         plt.savefig(outfile, bbox_inches='tight', pad_inches=0)
 
+def get_data():
+    return [('data/moustaches/*.png', Moustache), ('data/hats/*.png', Hat), ('data/eyes/*.png', Eyes)]
+    
 def update_image(infile, url):
     faces = detect_faces(url)
     if faces is None:
         return None
-    data = [('data/moustaches/*.png', Moustache), ('data/hats/*.png', Hat), ('data/eyes/*.png', Eyes)]
     f = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
     outfile = f.name
-    draw_on_face(infile, faces, data, outfile)
+    draw_on_face(infile, faces, get_data(), outfile)
     return outfile
 
 def test0():
-    data = [('data/moustaches/*.png', Moustache), ('data/hats/*.png', Hat), ('data/eyes/*.png', Eyes)]
-    # data = [('data/eyes/*.png', Eyes)]
-        # ('data/eye.npz', Eyes)]
-    url = 'http://pbs.twimg.com/media/DAMpysCUMAE3SlL.jpg'
-    url = 'https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/553988_3811036804045_1886663415_n.jpg?oh=c117d5b58989aec57e95cb797f7e2f2d&oe=59A39B93'
-    # url = 'https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/207146_1003361253911_9728_n.jpg?oh=77696385c0ed2002eb50f1c32bfd67f2&oe=59AB05A4'
-    faces = detect_faces(url)
-
     import cPickle
-    cPickle.dump(faces, open('data/tmp.pickle', 'w'))
-    # faces = cPickle.load(open('data/tmp.pickle'))  
-    infile = 'data/example.jpg'
-    draw_on_face(infile, faces, data)#, outfile='data/tmp.png')
+    import urllib
+    url = 'http://pbs.twimg.com/media/DAMpysCUMAE3SlL.jpg'
+    # url = 'https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/553988_3811036804045_1886663415_n.jpg?oh=c117d5b58989aec57e95cb797f7e2f2d&oe=59A39B93'
+    # url = 'https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/207146_1003361253911_9728_n.jpg?oh=77696385c0ed2002eb50f1c32bfd67f2&oe=59AB05A4'
+
+    # faces = detect_faces(url)
+    # cPickle.dump(faces, open('data/tmp.pickle', 'w'))
+    faces = cPickle.load(open('data/tmp.pickle'))  
+    
+    infile, _ = urllib.urlretrieve(url)
+    draw_on_face(infile, faces, get_data())#, outfile='data/tmp.png')
     
 def test1():
-    infile = 'data/example.jpg'
+    import urllib
     # url = 'http://pbs.twimg.com/media/DAMpysCUMAE3SlL.jpg'
     url = 'https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/553988_3811036804045_1886663415_n.jpg?oh=c117d5b58989aec57e95cb797f7e2f2d&oe=59A39B93'
+    infile, _ = urllib.urlretrieve(url)
     outfile = update_image(infile, url)
     img = io.imread(outfile)
     plt.imshow(img)
